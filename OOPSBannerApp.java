71604cd7cb71a84ecee0cc383973e4a9db9825f8
Mikul45 <ms1053@srmist.edu.in>
2026-03-25 11:39:30 +0530

    "UC8: OOPS banner using HashMap and render function"

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,62 @@
-public class OOPSBannerUC5 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerUC8 {
+
+    private static final Map<Character, String[]> patterns = new HashMap<>();
+
+    static {
+        patterns.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patterns.put('P', new String[]{
+                "***** ",
+                "*     *",
+                "*     *",
+                "***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patterns.put('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+    }
+
     public static void main(String[] args) {
+        renderBanner("OOPS");
+    }
 
-        String[] banner = {
-                String.join("", " *****   ", "*****   ", "*****   ", "***** "),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
-                String.join("", "*     * ", "*****   ", "*****   ", "***** "),
-                String.join("", "*     * ", "*       ", "*       ", "    * "),
-                String.join("", "*     * ", "*       ", "*       ", "    * "),
-                String.join("", " *****  ", "*       ", "*       ", "***** ")
-        };
+    public static void renderBanner(String word) {
 
-        for (String line : banner) {
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                String[] pattern = patterns.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append("   ");
+                }
+            }
+
             System.out.println(line);
         }
     }
