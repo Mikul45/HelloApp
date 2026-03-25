ef8cbde98df943a842f17c850f03c1cb88fd7ed3
Mikul45 <ms1053@srmist.edu.in>
2026-03-25 11:16:34 +0530

    "UC4: OOPS banner using array and loop"

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,23 @@
-
-    
-public class OOPSBannerApp {
-
+public class OOPSBannerUC4 {
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *****   ***** ");
-        System.out.println("*     * *     * *           * ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        // Step 1: Store banner lines in array
+        String[] banner = {
 
+                String.join("", " *****   ", "*****   ", "*****   ", "***** "),
+                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+                String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+                String.join("", "*     * ", "*****   ", "*****   ", "***** "),
+                String.join("", "*     * ", "*       ", "*       ", "    * "),
+                String.join("", "*     * ", "*       ", "*       ", "    * "),
+                String.join("", " *****  ", "*       ", "*       ", "***** ")
+
+        };
+
+        // Step 2: Print using loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
-
 }
+    
